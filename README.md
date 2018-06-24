b7853ebfa2609b50838bbf233062890de7de26e7
DasLampe <daslampe@lano-crew.org>
2018-06-24 22:43:07 +0200

    Move virtual to database

---
+++ README.md
@@ -0,0 +1,14 @@
+Config:
+
+```
+'postfix': {
+    'relayhost': '',
+    'networks': [],
+    'database': {
+        'username': 'vmail',
+        'password': '[generated]',
+        'db': 'vmail',
+        'host': '127.0.0.1',
+    },
+}
+```
--- items.py
+++ items.py
@@ -1,7 +1,12 @@
+# noinspection PyGlobalUndefined
+global node
+# noinspection PyGlobalUndefined
+global repo
+
+
 pkg_apt = {
     'postfix': {},
     'postfix-mysql': {},
-    'mariadb-server': {},
 }
 
 svc_systemv = {
@@ -12,7 +17,7 @@ svc_systemv = {
     },
     'mysql': {
         'needs': [
-            'pkg_apt:mariadb-server',
+            'pkg_apt:mysql-server',
         ],
     },
 }
@@ -20,10 +25,23 @@ svc_systemv = {
 directories = {
     '/etc/postfix/sasl': {
         'purge': True,
-    }
+    },
+    '/etc/postfix/virtual': {
+        'mode': '0640',
+        'owner': 'root',
+        'group': 'root',
+    },
 }
 
 files = {
+    #Cleanup
+    '/etc/postfix/main.cf.proto': {
+        'delete': True,
+    },
+    '/etc/postfix/master.cf.proto': {
+        'delete': True,
+    },
+
     "/etc/postfix/master.cf": {
         'source': 'etc/postfix/master.cf',
         'content_type': 'mako',
@@ -31,23 +49,62 @@ files = {
         'owner': 'root',
         'group': 'root',
     },
-    '/etc/postfix/main.cf.proto': {
-        'delete': True,
-    },
-    '/etc/postfix/master.cf.proto': {
-        'delete': True,
-    },
-    "/etc/postfix/virtual": {
-        'source': 'etc/postfix/virtual',
+
+    #Get accounts, redirect and some stuff from database
+    '/etc/postfix/virtual/accounts.cf': {
+        'source': 'etc/postfix/virtual/accounts.cf',
         'content_type': 'mako',
-        'mode': '0600',
+        'mode': '0640',
         'owner': 'root',
         'group': 'root',
-        'needs': ['pkg_apt:postfix'],
-        'triggers': {
-            'action:create_virtual_db',
-        }
     },
+    '/etc/postfix/virtual/aliases.cf': {
+        'source': 'etc/postfix/virtual/aliases.cf',
+        'content_type': 'mako',
+        'mode': '0640',
+        'owner': 'root',
+        'group': 'root',
+    },
+    '/etc/postfix/virtual/domains.cf': {
+        'source': 'etc/postfix/virtual/domains.cf',
+        'content_type': 'mako',
+        'mode': '0640',
+        'owner': 'root',
+        'group': 'root',
+    },
+    '/etc/postfix/virtual/recipient-access.cf': {
+        'source': 'etc/postfix/virtual/recipient-access.cf',
+        'content_type': 'mako',
+        'mode': '0640',
+        'owner': 'root',
+        'group': 'root',
+    },
+    '/etc/postfix/virtual/sender-login-maps.cf': {
+        'source': 'etc/postfix/virtual/sender-login-maps.cf',
+        'content_type': 'mako',
+        'mode': '0640',
+        'owner': 'root',
+        'group': 'root',
+    },
+    '/etc/postfix/virtual/tls-policy.cf': {
+        'source': 'etc/postfix/virtual/tls-policy.cf',
+        'content_type': 'mako',
+        'mode': '0640',
+        'owner': 'root',
+        'group': 'root',
+    },
+
+    # Place SQL-Structure file
+    '/etc/postfix/sql/structure.sql': {
+        'source': 'etc/postfix/sql/structure.sql',
+        'content_type': 'mako',
+        'mode': '640',
+        'owner': 'root',
+        'group': 'root',
+        'triggers': {
+            'action:deploy_postfix_mysql_structure',
+        }
+    }
 }
 
 if node.metadata.get('postfix', {}).get('relayhost' ''):
@@ -69,12 +126,40 @@ else:
         'needs': ['pkg_apt:postfix'],
     }
 
-actions = {
-    'create_virtual_db': {
-        'command': 'postmap /etc/postfix/virtual',
-        'triggered': True,
-        'triggers': {
-            #'svc_systemv:postfix:restart'
+# Create Database and user
+mysql_user = node.metadata.get('postfix', {}).get('database', {}).get('user', 'vmail')
+mysql_password = node.metadata.get('postfix', {}).get('database', {}).\
+    get('password', repo.libs.pw.get("mysql_{}_user_{}".format(mysql_user, node.name)))
+mysql_host = node.metadata.get('postfix', {}).get('database', {}).get('host', 'localhost')
+mysql_db = node.metadata.get('postfix', {}).get('database', {}).get('db', 'vmail')
+
+if mysql_host == 'localhost' or mysql_host == '127.0.0.1':
+    mysql_users = {
+        mysql_user: {
+            'password': mysql_password,
+            'hosts': ['127.0.0.1', '::1', 'localhost'].copy(),
+            'db_priv': {
+                mysql_db: 'all',
+            },
         },
+    }
+
+    mysql_dbs = {
+        mysql_db: {
+            'triggers': {
+                'action:deploy_postfix_mysql_structure',
+            },
+        },
+    }
+
+actions = {
+    'deploy_postfix_mysql_structure': {
+        'command': 'cat /etc/postfix/sql/structure.sql | mysql --defaults-file=/etc/mysql/debian.cnf',
+        'expected_return_code': 0,
+        'triggered': True,
+        'needs': [
+            'bundle:mysql',
+            'pkg_apt:mysql-server',
+        ],
     },
 }
